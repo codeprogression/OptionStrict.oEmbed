e9c7f778c7ca4297c4dd639a54ae48f8166d0fd9
Richard Cirerol <richard@codeprogression.com>
2011-07-28 14:43:58 -0600

    updating mvc version to have oembedrequest helper

--- src/OptionStrict.oEmbed.MVC/Extensions.cs
+++ src/OptionStrict.oEmbed.MVC/Extensions.cs
@@ -15,6 +15,16 @@ namespace OptionStrict.oEmbed.MVC
             return oEmbedReader.Read(api, url).oEmbed;
         }
 
+        public static oEmbed oEmbed(this HtmlHelper helper, oEmbedRequest request)
+        {
+            var oEmbedReader = DependencyResolver.Current.GetService<IoEmbedReader>();
+
+            if (oEmbedReader == null)
+                throw new NullOEmbedReaderException();
+            
+            return oEmbedReader.Read(request).oEmbed;
+        }
+
         public static oEmbed oEmbed(this HtmlHelper helper, Uri api, Uri url)
         {
             return oEmbed(helper,
